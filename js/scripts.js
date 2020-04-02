adbdb45dd44762b921247b7002d328947fc9e164
ashok thakur <iashok180@gmail.com>
2020-04-03 01:01:41 +0530

    table & map div added

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,13 @@ $(window).on('load', function (){
 })
 
 function initialize(){
-    $('.prev-btn').addClass('op-0')
     $.get('/state_wise', function(res){
         var overview_tmplt = _.template($("#overview-sc").html());
         var overview_html = overview_tmplt({ overview: res.response[0]});
         $("#overview").html(overview_html);
-        state_data = _.orderBy(res.response.splice(1), ['Confirmed'], ['desc']) 
+        state_data = _.orderBy(res.response.splice(1), ['Confirmed'], ['desc'])
         renderTable(state_data.slice(table_index['start_index'], table_index['end_index']))
+        $('#prev-btn').addClass('op-0')
     })
 }
 
@@ -30,21 +30,21 @@ $(document).ready(function(){
 })
 
 $('body')
-.on('click', '.next-btn', function(){
-    $('.prev-btn').removeClass('op-0')
+.on('click', '#next-btn', function(){
+    $('#prev-btn').removeClass('op-0')
     table_index['start_index'] += 10
     table_index['end_index'] += 10
-    if(table_index['start_index']<state_data.length){ 
-        if(table_index['end_index']>state_data.length) $('.next-btn').addClass('op-0')
+    if(table_index['start_index']<state_data.length){
+        if(table_index['end_index']>state_data.length) $('#next-btn').addClass('op-0')
         renderTable(state_data.slice(table_index['start_index'], table_index['end_index']))
     }
 })
-.on('click', '.prev-btn', function(){
-    $('.next-btn').removeClass('op-0')
+.on('click', '#prev-btn', function(){
+    $('#next-btn').removeClass('op-0')
     table_index['start_index'] -= 10
     table_index['end_index'] -= 10
     if (table_index['start_index']>=0){
-        if(table_index['start_index']==0) $('.prev-btn').addClass('op-0')
+        if(table_index['start_index']==0) $('#prev-btn').addClass('op-0')
         renderTable(state_data.slice(table_index['start_index'], table_index['end_index']))
     }
 })
