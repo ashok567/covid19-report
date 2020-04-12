8d54f9bb65e3691c1b9b9f222839bae92675baef
ashok thakur <iashok180@gmail.com>
2020-04-12 21:07:44 +0530

    delta value added

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
-import pandas as pd 
+import pandas as pd
 import requests
-from io import StringIO, BytesIO
+from io import BytesIO
 import yaml
 
 with open('config.yml', 'r') as config_file:
@@ -9,10 +9,13 @@ with open('config.yml', 'r') as config_file:
     except yaml.YAMLError as exc:
         print(exc)
 
+
 def statewise_count():
     response = requests.get(config['url'])
     data = response.content
-    # df1 = pd.read_excel(BytesIO(data), sheet_name='Raw_Data', encoding='utf-8')
-    statewise_df = pd.read_excel(BytesIO(data), sheet_name='Statewise', encoding='utf-8')
+    statewise_df = pd.read_excel(BytesIO(data),
+                                 sheet_name='Statewise', encoding='utf-8')
+    statewise_df['Delta_Total'] = statewise_df['Delta_Confirmed']
+    + statewise_df['Delta_Recovered'] + statewise_df['Delta_Deaths']
     statewise_df = statewise_df.to_json(orient='records')
     return statewise_df
